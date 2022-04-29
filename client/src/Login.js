85be704bf6e0ce0d0dc92c20539f5ffc8f8249b9
Xinrui Chen <xynree@gmail.com>
2022-04-29 16:33:08 -0700

    [ADD] Basic flexbox grid layout set up for Login Page to be replicated across Signup.

--- client/src/Login.js
+++ client/src/Login.js
@@ -26,22 +26,62 @@ const Login = ({ user, login }) => {
     if (user && user.id) history.push('/home');
   }, [user, history]);
 
+  const chatDisplayOverlay = {
+    width:'100%',
+    position: 'relative',
+    height:'100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent:'center',
+    backgroundImage: `url('/assets/bg-img.png')`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'top',
+    bgcolor: 'primary.main',
+    backgroundBlendMode: 'soft-light',
+    '&::after': {
+      content: '""',
+      opacity: 0.7,
+      top:0,
+      left:0,
+      bottom:0,
+      right:0,
+      position: 'absolute',
+    }
+  }
+
+
   return (
-    <Grid container justifyContent="center">
-      <Box>
-        <Grid container item>
-          <Typography>Need to register?</Typography>
-          <Link href="/register" to="/register">
-            <Button>Register</Button>
-          </Link>
-        </Grid>
+    <Grid container justifyContent="space-between" alignItems="center">
+      <Grid item xs={5}>
+        <Box sx={chatDisplayOverlay}>
+            <img alt='chat' src='/assets/bubble.svg'/>
+            <Typography>
+              Converse with anyone with any language
+            </Typography>
+        </Box>
+      </Grid>
+      <Grid item xs={7} direction="column" alignItems="center" justifyContent="center">
+        <Box sx={{
+          display: 'flex', flexDirection: 'column', 'width': '100%', 'height': '100vh', justifyContent: 'center', alignItems: 'center'
+          }}>
+          <Box sx={{display: 'flex', gap: '18px', width: '100%', height: '20%', boxSizing: 'border-box', padding: '48px', margin: 'auto', justifyContent: 'flex-end'}}>
+            <Typography >Don't have an account?</Typography>
+            <Link href="/register" to="/register">
+              <Button color="secondary" variant="contained" size='large'>Create Account</Button>
+            </Link>
+          </Box>
+
+        <Box sx={{margin: 'auto', padding: '36px', height: '60%'}}>
+        <Typography>Welcome Back!</Typography>
         <form onSubmit={handleLogin}>
-          <Grid>
+          <Grid item>
             <Grid>
               <FormControl margin="normal" required>
                 <TextField
                   aria-label="username"
-                  label="Username"
+                  label="username"
                   name="username"
                   type="text"
                 />
@@ -56,13 +96,15 @@ const Login = ({ user, login }) => {
               />
             </FormControl>
             <Grid>
-              <Button type="submit" variant="contained" size="large">
+              <Button color="primary" type="submit" variant="contained" size="large">
                 Login
               </Button>
             </Grid>
           </Grid>
         </form>
-      </Box>
+        </Box>
+        </Box>
+      </Grid>
     </Grid>
   );
 };
