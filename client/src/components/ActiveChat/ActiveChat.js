4f2eb2297a6facc015d7ef86bb0a9afaf6d8089a
Xinrui Chen <xynree@gmail.com>
2022-05-01 21:46:25 -0700

    This reverts commit 7211091893a9cccc3cd7378bfbce05673e6f368d.

--- client/src/components/ActiveChat/ActiveChat.js
+++ client/src/components/ActiveChat/ActiveChat.js
@@ -1,31 +1,71 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import { Box } from '@material-ui/core';
-import { SenderBubble, OtherUserBubble } from '.';
-import moment from 'moment';
+import { Input, Header, Messages } from './index';
 
-const Messages = (props) => {
-  const { messages, otherUser, userId } = props;
+const useStyles = makeStyles(() => ({
+  root: {
+    display: 'flex',
+    flexGrow: 8,
+    flexDirection: 'column',
+  },
+  chatContainer: {
+    marginLeft: 41,
+    marginRight: 41,
+    display: 'flex',
+    flexDirection: 'column',
+    flexGrow: 1,
+    justifyContent: 'space-between',
+  },
+}));
+
+const ActiveChat = ({
+  user,
+  conversations,
+  activeConversation,
+  postMessage,
+}) => {
+  const classes = useStyles();
+
+  const conversation = conversations
+    ? conversations.find(
+        (conversation) => conversation.otherUser.username === activeConversation
+      )
+    : {};
+
+  const isConversation = (obj) => {
+    return obj !== {} && obj !== undefined;
+  };
 
   return (
-    <Box>
-      {messages
-        .map((message) => {
-        const time = moment(message.createdAt).format('h:mm');
-
-        return message.senderId === userId ? (
-          <SenderBubble key={message.id} text={message.text} time={time} />
-        ) : (
-          <OtherUserBubble
-            key={message.id}
-            text={message.text}
-            time={time}
-            otherUser={otherUser}
+    <Box className={classes.root}>
+      {isConversation(conversation) && conversation.otherUser && (
+        <>
+          <Header
+            username={conversation.otherUser.username}
+            online={conversation.otherUser.online || false}
           />
-        );
-      })
-      }
+          <Box className={classes.chatContainer}>
+            {user && (
+              <>
+                <Messages
+                  messages={conversation.messages}
+                  otherUser={conversation.otherUser}
+                  userId={user.id}
+                />
+                <Input
+                  otherUser={conversation.otherUser}
+                  conversationId={conversation.id || null}
+                  user={user}
+                  postMessage={postMessage}
+                />
+              </>
+            )}
+          </Box>
+        </>
+      )}
     </Box>
   );
 };
 
-export default Messages;
+export default ActiveChat;
